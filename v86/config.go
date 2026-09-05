0599147b5286b601ddf2279be23c11cc2292537c
Broken Pipe <navigaid@gmail.com>
2026-09-06 02:57:56 +0800

    fix: remove empty aname from v86 root mount
    Linux 7.2.3 rejects an explicit empty 9P mount name before the host filesystem handler receives requests. Keep the host9p root options compatible with the updated guest kernel.

--- v86/config.go
+++ v86/config.go
@@ -94,7 +94,7 @@ func parseFlags(args []string) (map[string]any, error) {
 			"rw",
 			"root=host9p",
 			"rootfstype=9p",
-			"rootflags=trans=virtio,version=9p2000.L,aname=,cache=none,msize=131072",
+			"rootflags=trans=virtio,version=9p2000.L,cache=none,msize=131072",
 		}
 		cmdline += " " + strings.Join(fsargs, " ")
 		// up to caller to set up filesystem (handle9p)
